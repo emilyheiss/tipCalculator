1e40562f3dfcb8b663cd66bf1a11fa0ed3cc2243
Emily Heiss <emilyaheiss@gmail.com>
2019-10-05 18:52:37 -0400

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Emily Heiss on 10/5/19.
+//  Copyright © 2019 Heiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
